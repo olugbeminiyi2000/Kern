30d5f2689b3b1f2a08c5d5a4493962965b5b92cc
Emmanuel Obolo <e.obolo@alustudent.com>
2026-01-02 16:04:58 +0200

    updated the ker engine to be set up properly for linux and old setup tools

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup()
+setup(
+    name="kern-engine",
+    version="0.1.0",
+    description="A deep-reloading development engine for Python",
+    author="Emmanuel Obolo Oluwapelumi & Abiodun Kumuyi",
+    packages=find_packages(include=["core", "core.*", "tracker", "tracker.*", "hot_reload", "hot_reload.*", "utils", "utils.*"]),
+    entry_points={
+        "console_scripts": [
+            "kern=core.main:main",
+        ],
+    },
+)
